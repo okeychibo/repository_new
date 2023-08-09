799e4976025712838731a4e41478f09bef4d8433
miracle <o_chibo@yahoo.com>
2023-08-09 19:48:22 +0100

    second commit

--- data_ingest.py
+++ data_ingest.py
@@ -9,7 +9,7 @@ month = ['01']
 for data in month:
     result = wget.download(f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-{data}.parquet")
     final_df.append(result)
-    df = pd.read_parquet(result)
+    df = pd.read_parquet(result)                                                                           
 
 # change date from object to datetime
 df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
@@ -20,3 +20,6 @@ engine = create_engine('postgresql://postgres:root@localhost:5432/ayo_db')
 df_iter = df.iloc[:100000,:]
 
 df_iter.to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+
+
+# gggg
